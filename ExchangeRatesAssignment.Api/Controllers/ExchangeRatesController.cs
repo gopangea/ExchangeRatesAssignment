a51940f647a76c3b10fbcc68bc118c91c88145f4
Keerthi Maryala <keerthi@gopangea.com>
2024-04-18 13:06:50 -0500

    Skeleton Classes

---
+++ ExchangeRatesAssignment.Api/Controllers/ExchangeRatesController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using ExchangeRatesAssignment.Api;
+
+namespace ExchangeRatesAssignment.Api.Controllers
+{
+    [ApiController]
+    public class ExchangeRatesController : ControllerBase
+    {
+        // TODO : Implement an API endpoint that takes a three letter country code and returns a collection of
+        // calculated exchange rates of the type Api.Contracts.PangeaExchangeRate
+    }
+}
---
+++ ExchangeRatesAssignment.Api/Program.cs
@@ -0,0 +1,29 @@
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+{
+    var services = builder.Services;
+    services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
+}
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
